e0b2a51b595b68fef0b1030f85f5cdc3c504f1e8
gabrielmaedee <mamede.dev@gmail.com>
2025-07-26 15:57:07 -0300

    aula 06

---
+++ src/aula06/ControleRemoto.java
@@ -0,0 +1,118 @@
+package aula06;
+
+public class ControleRemoto implements Controlador{
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.isLigado());
+        System.out.println("Está tocando? " + this.isTocando());
+        System.out.println("Volume: " + this.getVolume());
+
+        for (int i = 0; i <= this.getVolume(); i += 10) {
+            System.out.print("O");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu...");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("tv está desligada...");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        } else {
+            System.out.println("tv está desligada...");
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.isLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        } else {
+            System.out.println("tv está desligada...");
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.isLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        } else {
+            System.out.println("tv está desligada...");
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.isLigado() && !(this.isTocando())) {
+            this.setTocando(true);
+        } else {
+            System.out.println("Já está tocando!");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.isLigado() && this.isTocando()) {
+            this.setTocando(false);
+        } else {
+            System.out.println("Já está pausado");
+        }
+
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+}
